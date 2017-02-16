57eb4d31643bacd937d68cecae3d33abe95c0fbd
Guenter Bartsch <guenter@zamia.org>
2017-02-16 11:10:46 +0100

    adapt to new infrastructure

--- speech_lex_review.py
+++ speech_lex_review.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- 
 
 #
-# Copyright 2013, 2014, 2016 Guenter Bartsch
+# Copyright 2013, 2014, 2016, 2017 Guenter Bartsch
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -31,7 +31,7 @@ import random
 
 from optparse import OptionParser
 
-import utils
+from nltools import misc
 
 LANG = 'de'
 
