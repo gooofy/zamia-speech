ed321705dcc02e4b6bb93ecf0584669f6de3cea3
Guenter Bartsch <guenter@zamia.org>
2017-10-13 00:25:55 +0200

    add noise phone to etc/voxforge.phone

--- speech_sphinx_export.py
+++ speech_sphinx_export.py
@@ -183,6 +183,7 @@ def export_sphinx_case(work_dir, sphinxtrain_cfg_fn):
             phf.write (u'%s\n' % phone)
 
         phf.write (u'SIL\n')
+        phf.write (u'#\n')
 
     logging.info("%s written." % phfn)
 
