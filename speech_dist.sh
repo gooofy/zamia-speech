dbed2843a8c9d033ae5eb68db5c5238ace680795
Guenter Bartsch <guenter@zamia.org>
2017-11-29 21:05:35 +0100

    nnet3 models are deprecated now

--- speech_dist.sh
+++ speech_dist.sh
@@ -71,60 +71,6 @@ rm -r "$DISTDIR/$AMNAME"
 exit 0
 
 #
-# kaldi nnet3 models 
-#
-
-AMNAME="kaldi-nnet3-voxforge-${LANG}-r$datum"
-
-mkdir "$DISTDIR/$AMNAME"
-
-function export_kaldi_nnet3 {
-
-    EXPNAME=$1
-    GRAPHNAME=$2
-
-    mkdir "$DISTDIR/$AMNAME/$EXPNAME"
-
-    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$EXPNAME/final.mdl                  $DISTDIR/$AMNAME/$EXPNAME/
-    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$EXPNAME/cmvn_opts                  $DISTDIR/$AMNAME/$EXPNAME/ 2>/dev/null 
-
-    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$GRAPHNAME/HCLG.fst                 $DISTDIR/$AMNAME/$EXPNAME/
-    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$GRAPHNAME/words.txt                $DISTDIR/$AMNAME/$EXPNAME/
-    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$GRAPHNAME/num_pdfs                 $DISTDIR/$AMNAME/$EXPNAME/
-    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$GRAPHNAME/phones/align_lexicon.int $DISTDIR/$AMNAME/$EXPNAME/
-
-}
-
-export_kaldi_nnet3 nnet_tdnn_a  nnet_tdnn_a/graph
-# export_kaldi_nnet3 lstm_ld5     lstm_ld5/graph
-
-mkdir "$DISTDIR/$AMNAME/extractor"
-
-cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/final.mat            "$DISTDIR/$AMNAME/extractor/"
-cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/global_cmvn.stats    "$DISTDIR/$AMNAME/extractor/"
-cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/final.dubm           "$DISTDIR/$AMNAME/extractor/"
-cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/final.ie             "$DISTDIR/$AMNAME/extractor/"
-cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/splice_opts          "$DISTDIR/$AMNAME/extractor/"
-cp data/dst/speech/${LANG}/kaldi/exp/nnet3/ivectors_test/conf/splice.conf "$DISTDIR/$AMNAME/extractor/"
-
-cp data/dst/speech/${LANG}/kaldi/RESULTS.txt $DISTDIR/$AMNAME/
-cp README.md "$DISTDIR/$AMNAME"
-cp LICENSE   "$DISTDIR/$AMNAME"
-cp AUTHORS   "$DISTDIR/$AMNAME"
-
-mkdir "$DISTDIR/$AMNAME/conf"
-cp data/src/speech/kaldi-mfcc.conf        $DISTDIR/$AMNAME/conf/mfcc.conf 
-cp data/src/speech/kaldi-mfcc-hires.conf  $DISTDIR/$AMNAME/conf/mfcc-hires.conf  
-cp data/src/speech/kaldi-online-cmvn.conf $DISTDIR/$AMNAME/conf/online_cmvn.conf
-
-pushd $DISTDIR
-tar cfv "$AMNAME.tar" $AMNAME
-xz -v -8 -T 12 "$AMNAME.tar"
-popd
-
-rm -r "$DISTDIR/$AMNAME"
-
-#
 # cont sphinx model
 #
 
@@ -207,4 +153,64 @@ cp AUTHORS   "$DISTDIR"
 #
 
 echo rsync -avPz --delete --bwlimit=256 data/dist/${LANG} goofy:/var/www/html/voxforge/
+
+#
+# FIXME: remove deprecated code below
+#
+
+exit 0
+
+#
+# kaldi nnet3 models 
+#
+
+AMNAME="kaldi-nnet3-voxforge-${LANG}-r$datum"
+
+mkdir "$DISTDIR/$AMNAME"
+
+function export_kaldi_nnet3 {
+
+    EXPNAME=$1
+    GRAPHNAME=$2
+
+    mkdir "$DISTDIR/$AMNAME/$EXPNAME"
+
+    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$EXPNAME/final.mdl                  $DISTDIR/$AMNAME/$EXPNAME/
+    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$EXPNAME/cmvn_opts                  $DISTDIR/$AMNAME/$EXPNAME/ 2>/dev/null 
+
+    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$GRAPHNAME/HCLG.fst                 $DISTDIR/$AMNAME/$EXPNAME/
+    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$GRAPHNAME/words.txt                $DISTDIR/$AMNAME/$EXPNAME/
+    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$GRAPHNAME/num_pdfs                 $DISTDIR/$AMNAME/$EXPNAME/
+    cp data/dst/speech/${LANG}/kaldi/exp/nnet3/$GRAPHNAME/phones/align_lexicon.int $DISTDIR/$AMNAME/$EXPNAME/
+
+}
+
+export_kaldi_nnet3 nnet_tdnn_a  nnet_tdnn_a/graph
+# export_kaldi_nnet3 lstm_ld5     lstm_ld5/graph
+
+mkdir "$DISTDIR/$AMNAME/extractor"
+
+cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/final.mat            "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/global_cmvn.stats    "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/final.dubm           "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/final.ie             "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/${LANG}/kaldi/exp/nnet3/extractor/splice_opts          "$DISTDIR/$AMNAME/extractor/"
+cp data/dst/speech/${LANG}/kaldi/exp/nnet3/ivectors_test/conf/splice.conf "$DISTDIR/$AMNAME/extractor/"
+
+cp data/dst/speech/${LANG}/kaldi/RESULTS.txt $DISTDIR/$AMNAME/
+cp README.md "$DISTDIR/$AMNAME"
+cp LICENSE   "$DISTDIR/$AMNAME"
+cp AUTHORS   "$DISTDIR/$AMNAME"
+
+mkdir "$DISTDIR/$AMNAME/conf"
+cp data/src/speech/kaldi-mfcc.conf        $DISTDIR/$AMNAME/conf/mfcc.conf 
+cp data/src/speech/kaldi-mfcc-hires.conf  $DISTDIR/$AMNAME/conf/mfcc-hires.conf  
+cp data/src/speech/kaldi-online-cmvn.conf $DISTDIR/$AMNAME/conf/online_cmvn.conf
+
+pushd $DISTDIR
+tar cfv "$AMNAME.tar" $AMNAME
+xz -v -8 -T 12 "$AMNAME.tar"
+popd
+
+rm -r "$DISTDIR/$AMNAME"
 
