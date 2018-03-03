dca5994615932e5fda127f8c6234b5582cb9b1d8
Guenter Bartsch <guenter@zamia.org>
2018-03-03 23:34:51 +0100

    use per-speaker split

--- speech_deepspeech_export.py
+++ speech_deepspeech_export.py
@@ -36,10 +36,8 @@ from nltools.tokenizer      import tokenize
 from speech_transcripts     import Transcripts
 
 WORKDIR             = 'data/dst/speech/%s/deepspeech'
-MIN_QUALITY         = 2
-P_DEV               = 5 # use 5% for dev dataset
-P_TEST              = 5 # use 5% for test dataset
 PROMPT_AUDIO_FACTOR = 1000
+
 #
 # init 
 #
@@ -83,17 +81,9 @@ wav16_dir   = config.get("speech", "wav16_dir_%s" % options.lang)
 logging.info ( "loading transcripts...")
 transcripts = Transcripts(lang=options.lang)
 logging.info ( "loading transcripts...done. %d transcripts." % len(transcripts))
-
-#
-# count good submissions
-#
-
-total_good = 0
-for cfn in transcripts:
-    ts = transcripts[cfn]
-    if ts['quality'] < MIN_QUALITY:
-        continue
-    total_good += 1
+logging.info ("splitting transcripts...")
+ts_all, ts_train, ts_test = transcripts.split()
+logging.info ("splitting transcripts done, %d train, %d test." % (len(ts_train), len(ts_test)))
 
 #
 # create work_dir 
@@ -101,8 +91,7 @@ for cfn in transcripts:
 
 misc.mkdirs('%s' % work_dir)
 
-# /home/bofh/projects/ai/mozilla/DeepSpeech/data/librivox/LibriSpeech/dev-clean-wav/2277-149897-0004.wav,183564,he was getting some vague comfort out of a good cigar but it was no panacea for the ill which affected him
-
+# export csv files
 
 csv_train_fn = '%s/train.csv' % work_dir
 csv_dev_fn   = '%s/dev.csv'   % work_dir
@@ -110,51 +99,40 @@ csv_test_fn  = '%s/test.csv'  % work_dir
 
 alphabet = set()
 
-with codecs.open(csv_train_fn, 'w', 'utf8') as csv_train_f, \
-     codecs.open(csv_dev_fn  , 'w', 'utf8') as csv_dev_f, \
-     codecs.open(csv_test_fn , 'w', 'utf8') as csv_test_f:
+def export_ds(ds, csv_fn):
 
-    csv_train_f.write('wav_filename,wav_filesize,transcript\n')
-    csv_dev_f.write('wav_filename,wav_filesize,transcript\n')
-    csv_test_f.write('wav_filename,wav_filesize,transcript\n')
+    global alphabet
 
-    cnt_train = 0
-    cnt_dev   = 0
-    cnt_test  = 0
-    cnt_all   = 0
+    cnt = 0
+    with codecs.open(csv_fn, 'w', 'utf8') as csv_f:
 
-    for cfn in transcripts:
-        ts = transcripts[cfn]
+        csv_f.write('wav_filename,wav_filesize,transcript\n')
 
-        if ts['quality'] < MIN_QUALITY:
-            continue
 
-        wavfn  = '%s/%s.wav' % (wav16_dir, cfn)
-        wavlen = os.path.getsize(wavfn)
-        prompt = ts['ts']
+        for cfn in ds:
+            ts = ds[cfn]
 
-        if (len(prompt)*PROMPT_AUDIO_FACTOR) > wavlen:
-            logging.warn('Skipping %s (wav (%d bytes) too short for prompt (%d chars)' % (cfn, wavlen, len(prompt)))
-            continue
+            wavfn  = '%s/%s.wav' % (wav16_dir, cfn)
+            wavlen = os.path.getsize(wavfn)
+            prompt = ts['ts']
 
-        for c in prompt:
-            alphabet.add(c)
+            if (len(prompt)*PROMPT_AUDIO_FACTOR) > wavlen:
+                logging.warn('Skipping %s (wav (%d bytes) too short for prompt (%d chars)' % (cfn, wavlen, len(prompt)))
+                continue
 
-        if cnt_dev < (cnt_all * P_DEV / 100):
-            csv_dev_f.write('%s,%d,%s\n' % (wavfn, wavlen, prompt))
-            cnt_dev += 1
-        elif cnt_test < (cnt_all * P_TEST / 100):
-            csv_test_f.write('%s,%d,%s\n' % (wavfn, wavlen, prompt))
-            cnt_test += 1
-        else:
-            csv_train_f.write('%s,%d,%s\n' % (wavfn, wavlen, prompt))
-            cnt_train += 1
+            for c in prompt:
+                alphabet.add(c)
 
-        cnt_all = cnt_dev + cnt_test + cnt_train
+            csv_f.write('%s,%d,%s\n' % (wavfn, wavlen, prompt))
+            cnt += 1
 
-logging.info ("%s %d lines written." % (csv_train_fn, cnt_train) )
-logging.info ("%s %d lines written." % (csv_dev_fn, cnt_dev) )
-logging.info ("%s %d lines written." % (csv_test_fn, cnt_test) )
+    logging.info ("%s %d lines written." % (csv_fn, cnt) )
+
+export_ds(ts_train, csv_train_fn)
+export_ds(ts_test,  csv_test_fn)
+export_ds([],       csv_dev_fn)
+
+# export alphabet
 
 alphabet_fn = '%s/alphabet.txt' % work_dir
 
@@ -163,5 +141,4 @@ with codecs.open(alphabet_fn, 'w', 'utf8') as alphabet_f:
     for c in sorted(alphabet):
         alphabet_f.write('%s\n' % c)
 logging.info ("%s written." % alphabet_fn)
-
 
