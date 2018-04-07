f5b914b6cf59a51d3b97834e83e13ca89212e835
Guenter Bartsch <guenter@zamia.org>
2018-04-08 00:44:21 +0200

    audiobook preprocessing tool added

---
+++ abook-preprocess-transcript.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#
+# Copyright 2014, 2018 Guenter Bartsch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys
+import os
+import codecs
+import traceback
+import datetime
+import logging
+import readline
+import wave, struct, array
+
+import numpy as np
+
+from optparse           import OptionParser
+from nltools            import misc
+from nltools.tokenizer  import tokenize
+from speech_lexicon     import Lexicon
+from lex_edit           import LexEdit
+
+#
+# tokenize transcript, check against lexicon for OOVs
+#
+
+PROC_TITLE = 'abook-preprocess-transcript'
+LANG       = 'de'
+
+#
+# init terminal
+#
+
+misc.init_app (PROC_TITLE)
+readline.set_history_length(1000)
+
+#
+# config
+#
+
+config = misc.load_config('.speechrc')
+
+#
+# command line
+#
+
+parser = OptionParser("usage: %prog [options] transcript.txt")
+
+parser.add_option("-v", "--verbose", action="store_true", dest="verbose", 
+                  help="enable debug output")
+
+(options, args) = parser.parse_args()
+
+if options.verbose:
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+else:
+    logging.basicConfig(level=logging.INFO)
+
+if len(args) != 1:
+    parser.print_usage()
+    sys.exit(1)
+
+inputfn        = args[0]
+outputfn       = 'prompts.txt'
+replfn         = 'repl.txt'
+
+#
+# load lexicon
+#
+
+logging.info("loading lexicon...")
+lex = Lexicon(lang=LANG)
+logging.info("loading lexicon...done.")
+
+lex_edit = LexEdit(lex)
+
+#
+# linecount
+#
+
+linetot = 0
+with codecs.open(inputfn, 'r', 'utf8') as inputf:
+    for line in inputf:
+        linetot += 1
+
+#
+# main
+#
+
+wrt     = {}
+linecnt = 0
+
+with codecs.open(inputfn, 'r', 'utf8') as inputf:
+    with codecs.open(outputfn, 'w', 'utf8') as outputf:
+        with codecs.open(replfn, 'w', 'utf8') as replf:
+
+            for line in inputf:
+
+                line = line.strip()
+                readline.add_history(line)
+                linecnt += 1
+
+                while True:
+
+                    oovs = set()
+                    for token in tokenize(line, lang='de'):
+                        token = wrt[token] if token in wrt else token
+                        # print repr(token)
+                        if not token in lex:
+                            oovs.add(token)
+
+                    out_tokens = []
+                    for token in tokenize(line, lang=LANG, keep_punctuation=True):
+                        token = wrt[token] if token in wrt else token
+                        out_tokens.append(token)
+
+                    outline = u" ".join(out_tokens)
+
+                    for t in wrt:
+                        outline = outline.replace(t, wrt[t])
+
+                    if not oovs:
+                        break
+
+                    oovs = sorted(list(oovs))
+                    oov = oovs[0]
+
+                    print
+                    print u"%4d/%4d: %s" % (linecnt, linetot, line)
+                    print u"%4d/%4d: %s" % (linecnt, linetot, outline)
+                    print
+                    print u"OOV: %s" % oov
+                    print
+                    
+                    resp = raw_input("E:Edit L:Lex R:Replace Q:Quit >" )
+
+                    if resp.lower() == 'q':
+                        sys.exit(0)
+                    elif resp.lower() == 'e':
+                        line = raw_input("Prompt> ")
+                    elif resp.lower() == 'r':
+                        oov_repl = raw_input("Replacement for %s> " % oov).decode('utf8')
+                        wrt[oov] = oov_repl
+                        replf.write(u"        u'%s'           : u'%s',\n" % (oov, wrt[oov]))
+                        replf.flush()
+                    elif resp.lower() == 'l':
+                        lex_edit.edit(oov)
+
+                outputf.write(u"%s\n" % outline)
+                outputf.flush()
+
+print "%s written." % outputfn
+print "%s written." % replfn
+
---
+++ lex_edit.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#
+# Copyright 2014, 2018 Guenter Bartsch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# lex-edit loop component, intended to be embedded into other scripts
+#
+
+import sys
+import os
+import traceback
+import logging
+import readline
+
+from nltools.tts            import TTS
+from nltools.sequiturclient import sequitur_gen_ipa
+from nltools.phonetics      import ipa2xsampa, xsampa2ipa
+
+SEQUITUR_MODEL    = 'data/models/sequitur-voxforge-de-latest'
+
+class LexEdit(object):
+
+    def __init__(self, lex):
+    
+        self.lex = lex
+
+        #
+        # TTS (for audio output)
+        #
+
+        self.tts = TTS ('local', 0, locale='de', voice='bits3', engine='espeak')
+
+    def lex_gen_ipa (self, lex_base, locale, engine, voice, speak=False):
+
+        ipas = u''
+        try:
+
+            if engine == 'sequitur':
+                ipas = sequitur_gen_ipa (SEQUITUR_MODEL, lex_base)
+            
+            else:
+                self.tts.locale = locale
+                self.tts.engine = engine
+                self.tts.voice  = voice
+                ipas = self.tts.gen_ipa (lex_base)
+
+            if speak:
+                self.tts.locale = 'de'
+                self.tts.engine = 'mary'
+                self.tts.voice  = 'dfki-pavoque-neutral-hsmm'
+                self.tts.say_ipa(ipas, async=True)
+
+        except:
+            logging.error('EXCEPTION CAUGHT %s' % traceback.format_exc())
+
+        return ipas
+
+    def edit(self, lex_token):
+
+        lex_base  = lex_token.split('_')[0]
+
+        if lex_token in self.lex:
+            lex_entry = lex[lex_token]
+
+        else:
+            ipas = self.lex_gen_ipa(lex_base, 'de', 'sequitur', 'de')
+            lex_entry = {'ipa': ipas}
+            self.lex[lex_token] = lex_entry
+
+        ipas = lex_entry['ipa']
+
+        try:
+            self.tts.locale ='de'
+            self.tts.engine ='mary'
+            self.tts.voice  ='dfki-pavoque-neutral-hsmm'
+            self.tts.say_ipa(ipas, async=True)
+        except:
+            logging.error('EXCEPTION CAUGHT %s' % traceback.format_exc())
+
+        lex_gen = {}
+
+        lex_gen['de-mary']     = self.lex_gen_ipa(lex_base, 'de', 'mary',     'bits3')
+        lex_gen['de-espeak']   = self.lex_gen_ipa(lex_base, 'de', 'espeak',   'de')
+        lex_gen['de-sequitur'] = self.lex_gen_ipa(lex_base, 'de', 'sequitur', 'de')
+
+        while True:
+
+            print
+            print u"Token       : %s" % lex_token
+            print u"IPA         : %s" % lex_entry['ipa']
+            print
+
+            for engine in sorted(lex_gen):
+                print u"%-11s : %s" % (engine, lex_gen[engine])
+            print
+
+            if lex_token in self.lex:
+                m = self.lex.get_multi(lex_token)
+                for k in m:
+                    print u"%s [%s]" % (k, m[k]['ipa'])
+
+            else:
+                print u"NEW TOKEN"
+
+            print u"SPEAK  P:de-unitsel  O:de-hsmm                   I:fr-hsmm   U:en-hsmm"
+            print u"GEN    G:de-mary     H:de-espeak  J:de-sequitur  K:fr-mary   L:en-mary"
+            print u"       E:Edit        Q:Quit "
+
+            try:
+
+                resp = raw_input("Lex> ")
+
+                # quit
+                if resp.lower() == 'q':
+                    break  
+            
+                # generate de-mary
+                elif resp.lower() == 'g':
+                    lex_entry['ipa'] = self.lex_gen_ipa (lex_base, 'de', 'mary', 'bits3', True)
+
+                # generate de-espeak
+                elif resp.lower() == 'h':
+                    lex_entry['ipa'] = self.lex_gen_ipa (lex_base, 'de', 'espeak', 'de', True)
+                    
+                # generate en-mary 
+                elif resp.lower() == 'l':
+                    
+                    self.tts.locale ='en-US'
+                    self.tts.engine ='mary'
+                    self.tts.voice  ='cmu-rms-hsmm'
+
+                    ipas = self.tts.gen_ipa (lex_base)
+                    self.tts.say_ipa(ipas, async=True)
+                    lex_entry['ipa'] = ipas
+
+                # generate fr-mary 
+                elif resp.lower() == 'k':
+                    
+                    self.tts.locale ='fr'
+                    self.tts.engine ='mary'
+                    self.tts.voice  ='upmc-pierre-hsmm'
+
+                    ipas = self.tts.gen_ipa (lex_base)
+                    self.tts.say_ipa(ipas, async=True)
+                    lex_entry['ipa'] = ipas
+
+                # generate de-sequitur
+                elif resp.lower() == 'j':
+                    lex_entry['ipa'] = self.lex_gen_ipa (lex_base, 'de', 'sequitur', 'de', True)
+                    
+                # speak de mary unitsel 
+                elif resp.lower() == 'p':
+            
+                    if len(lex_entry['ipa']) == 0:
+                        continue
+            
+                    ipas = lex_entry['ipa']
+
+                    self.tts.locale ='de'
+                    self.tts.engine ='mary'
+                    self.tts.voice  ='bits3'
+
+                    self.tts.say_ipa(ipas, async=True)
+
+                # speak de mary hsmm
+                elif resp.lower() == 'o':
+            
+                    if len(lex_entry['ipa']) == 0:
+                        continue
+            
+                    ipas = lex_entry['ipa']
+
+                    self.tts.locale = 'de'
+                    self.tts.engine = 'mary'
+                    self.tts.voice  = 'dfki-pavoque-neutral-hsmm'
+
+                    self.tts.say_ipa(ipas, async=True)
+
+                # speak fr mary hsmm
+                elif resp.lower() == 'i':
+           
+                    if len(lex_entry['ipa']) == 0:
+                        continue
+            
+                    ipas = lex_entry['ipa']
+
+                    self.tts.locale ='fr'
+                    self.tts.engine ='mary'
+                    self.tts.voice  ='upmc-pierre-hsmm'
+
+                    self.tts.say_ipa(ipas, async=True)
+           
+                # speak en mary hsmm
+                elif resp.lower() == 'u':
+            
+                    ipas = lex_entry['ipa']
+
+                    self.tts.locale = 'en-US'
+                    self.tts.engine = 'mary'
+                    self.tts.voice  = 'cmu-rms-hsmm'
+
+                    self.tts.say_ipa(ipas, async=True)
+           
+                # edit XS
+                elif resp.lower() == 'e':
+            
+                    ipas = lex_entry['ipa']
+
+                    xs = ipa2xsampa (lex_token, ipas, stress_to_vowels=False)
+                    readline.add_history(xs)
+                    xs = raw_input(xs + '> ')
+
+                    ipas = xsampa2ipa (lex_token, xs)
+        
+                    lex_entry['ipa'] = ipas
+
+            except:
+                logging.error('EXCEPTION CAUGHT %s' % traceback.format_exc())
+
+        self.lex.save()
+        print "new lexicon saved."
+        print
+
+
+
