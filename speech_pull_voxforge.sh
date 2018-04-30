d424282b068aa95533ee268cc463819007223aa3
Guenter Bartsch <guenter@zamia.org>
2018-04-30 20:54:07 +0200

    mkdir added

--- speech_pull_voxforge.sh
+++ speech_pull_voxforge.sh
@@ -7,11 +7,13 @@ SPEECH_CORPORA=/home/bofh/projects/ai/data/speech/corpora
 # english
 #
 
+mkdir -p "${SPEECH_ARCH}/voxforge_en"
 cd "${SPEECH_ARCH}/voxforge_en"
 
 rm index.*
 wget -c -r -nd -l 1 -np http://www.repository.voxforge1.org/downloads/SpeechCorpus/Trunk/Audio/Main/16kHz_16bit/
 
+mkdir -p "${SPEECH_CORPORA}/voxforge_en"
 cd "${SPEECH_CORPORA}/voxforge_en"
 for i in ${SPEECH_ARCH}/voxforge_en/*.tgz ; do
 
@@ -24,12 +26,13 @@ done
 # german
 #
 
+mkdir -p "${SPEECH_ARCH}/voxforge_de"
 cd "${SPEECH_ARCH}/voxforge_de"
 
 rm index.*
 wget -c -r -nd -l 1 -np http://www.repository.voxforge1.org/downloads/de/Trunk/Audio/Main/16kHz_16bit/
-# rm openpento*
 
+mkdir -p "${SPEECH_CORPORA}/voxforge_de"
 cd "${SPEECH_CORPORA}/voxforge_de"
 for i in ${SPEECH_ARCH}/voxforge_de/*.tgz ; do
 
