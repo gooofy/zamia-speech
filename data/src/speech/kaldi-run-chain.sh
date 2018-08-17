55e728ae6ff0673336376df690646d165f6702e5
Guenter Bartsch <guenter@zamia.org>
2018-08-17 19:08:27 +0200

    add larger tdnn_f chain setup

--- data/src/speech/kaldi-run-chain.sh
+++ data/src/speech/kaldi-run-chain.sh
@@ -26,10 +26,13 @@ train_set=train
 gmm=tri2b_chain # the gmm for the target data
 nnet3_affix=_chain  # cleanup affix for nnet3 and chain dirs, e.g. _cleaned
 num_threads_ubm=12
+get_egs_stage=-10
 
 xent_regularize=0.1
 train_stage=-10
 common_egs_dir=  # you can set this to use previously dumped egs.
+dropout_schedule='0,0@0.20,0.5@0.50,0'
+frames_per_eg=150,110,100
 
 # pre-flight checks
 
@@ -60,7 +63,7 @@ fi
 . utils/parse_options.sh
 
 echo "Runtime configuration is: nJobs $nJobs, nDecodeJobs $nDecodeJobs. If this is not what you want, edit cmd.sh"
-echo "Starting at stage $stage"
+echo "Starting at stage $stage, train_stage $train_stage"
 
 # now start preprocessing with KALDI scripts
 
@@ -210,6 +213,7 @@ if [ $stage -le 9 ]; then
     steps/nnet3/chain/build_tree.sh --frame-subsampling-factor 3 \
         --context-opts "--context-width=2 --central-position=1" \
         --cmd "$train_cmd" 4000 ${lores_train_data_dir} data/lang_chain $ali_dir $tree_dir
+    # FIXME: 7000 leafs
 
     mkdir -p $dir
 
@@ -399,6 +403,125 @@ EOF
       --tree-dir $tree_dir \
       --lat-dir $lat_dir \
       --dir $dir
+
+    echo
+    echo mkgraph
+    echo
+
+    utils/mkgraph.sh --self-loop-scale 1.0 data/lang_test $dir $dir/graph
+
+    echo
+    echo decode
+    echo
+
+    steps/nnet3/decode.sh --num-threads 1 --nj $nDecodeJobs --cmd "$decode_cmd" \
+                          --acwt 1.0 --post-decode-acwt 10.0 \
+                          --online-ivector-dir exp/nnet3${nnet3_affix}/ivectors_test_hires \
+                          --scoring-opts "--min-lmwt 5 " \
+                          $dir/graph data/test_hires $dir/decode_test || exit 1;
+
+    grep WER $dir/decode_test/scoring_kaldi/best_wer >>RESULTS.txt
+
+fi
+
+#
+# larger tdnn_f model for higher end machines
+#
+# network config based on 
+# egs/swbd/s5c/local/chain/tuning/run_tdnn_7q.sh
+#
+
+dir=exp/nnet3${nnet3_affix}/tdnn_f
+
+num_targets=$(tree-info $tree_dir/tree |grep num-pdfs|awk '{print $2}')
+learning_rate_factor=$(echo "print 0.5/$xent_regularize" | python)
+affine_opts="l2-regularize=0.01 dropout-proportion=0.0 dropout-per-dim=true dropout-per-dim-continuous=true"
+tdnnf_opts="l2-regularize=0.01 dropout-proportion=0.0 bypass-scale=0.66"
+linear_opts="l2-regularize=0.01 orthonormal-constraint=-1.0"
+prefinal_opts="l2-regularize=0.01"
+output_opts="l2-regularize=0.002"
+
+if [ $stage -le 14 ]; then
+
+    mkdir -p $dir
+
+    echo
+    echo "$0: creating neural net configs using the xconfig parser";
+    echo
+
+    mkdir -p $dir/configs
+    cat <<EOF > $dir/configs/network.xconfig
+input dim=100 name=ivector
+input dim=40 name=input
+
+# please note that it is important to have input layer with the name=input
+# as the layer immediately preceding the fixed-affine-layer to enable
+# the use of short notation for the descriptor
+fixed-affine-layer name=lda input=Append(-1,0,1,ReplaceIndex(ivector, t, 0)) affine-transform-file=$dir/configs/lda.mat
+
+# the first splicing is moved before the lda layer, so no splicing here
+relu-batchnorm-dropout-layer name=tdnn1 $affine_opts dim=1536
+tdnnf-layer name=tdnnf2 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=1
+tdnnf-layer name=tdnnf3 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=1
+tdnnf-layer name=tdnnf4 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=1
+tdnnf-layer name=tdnnf5 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=0
+tdnnf-layer name=tdnnf6 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf7 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf8 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf9 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf10 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf11 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf12 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf13 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf14 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+tdnnf-layer name=tdnnf15 $tdnnf_opts dim=1536 bottleneck-dim=160 time-stride=3
+linear-component name=prefinal-l dim=256 $linear_opts
+
+prefinal-layer name=prefinal-chain input=prefinal-l $prefinal_opts big-dim=1536 small-dim=256
+output-layer name=output include-log-softmax=false dim=$num_targets $output_opts
+
+prefinal-layer name=prefinal-xent input=prefinal-l $prefinal_opts big-dim=1536 small-dim=256
+output-layer name=output-xent dim=$num_targets learning-rate-factor=$learning_rate_factor $output_opts
+
+EOF
+    steps/nnet3/xconfig_to_configs.py --xconfig-file $dir/configs/network.xconfig --config-dir $dir/configs/
+
+fi
+
+if [ $stage -le 15 ]; then
+
+    echo
+    echo train.py 
+    echo
+
+    steps/nnet3/chain/train.py --stage $train_stage \
+      --cmd "$decode_cmd" \
+      --feat.online-ivector-dir $train_ivector_dir \
+      --feat.cmvn-opts "--norm-means=false --norm-vars=false" \
+      --chain.xent-regularize $xent_regularize \
+      --chain.leaky-hmm-coefficient 0.1 \
+      --chain.l2-regularize 0.0 \
+      --chain.apply-deriv-weights false \
+      --chain.lm-opts="--num-extra-lm-states=2000" \
+      --trainer.dropout-schedule $dropout_schedule \
+      --trainer.add-option="--optimization.memory-compression-level=2" \
+      --egs.dir "$common_egs_dir" \
+      --egs.stage $get_egs_stage \
+      --egs.opts "--frames-overlap-per-eg 0 --constrained false" \
+      --egs.chunk-width $frames_per_eg \
+      --trainer.num-chunk-per-minibatch 320 \
+      --trainer.frames-per-iter 1500000 \
+      --trainer.num-epochs 6 \
+      --trainer.optimization.num-jobs-initial 1 \
+      --trainer.optimization.num-jobs-final 1 \
+      --trainer.optimization.initial-effective-lrate 0.00025 \
+      --trainer.optimization.final-effective-lrate 0.000025 \
+      --trainer.max-param-change 2.0 \
+      --cleanup.remove-egs true \
+      --feat-dir $train_data_dir \
+      --tree-dir $tree_dir \
+      --lat-dir $lat_dir \
+      --dir $dir  || exit 1;
 
     echo
     echo mkgraph
