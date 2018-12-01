5e632b862ba944d892f1b7445c5fdebdbe77be61
Guenter Bartsch <guenter@zamia.org>
2018-12-01 01:25:16 +0100

    fix uttid

--- mailabs_to_vf.py
+++ mailabs_to_vf.py
@@ -129,7 +129,7 @@ with open('tmp/run_parallel.sh', 'w') as scriptf:
                         for wavfn in meta:
 
                             ts_orig = meta[wavfn]['clean']
-                            uttid = wavfn.replace('_','-')
+                            uttid = os.path.splitext(wavfn.replace('_','-'))[0]
 
                             if uttid in all_utts:
                                 logging.error('utterance id not unique:' % uttid)
@@ -138,7 +138,7 @@ with open('tmp/run_parallel.sh', 'w') as scriptf:
 
                             wav_in = '%s/%s/by_book/%s/%s/%s/wavs/%s' % (srcdir, localedir, gender, speaker, book, wavfn)
 
-                            wav_out = '%s/wav/%s' % (dstdir, uttid)
+                            wav_out = '%s/wav/%s.wav' % (dstdir, uttid)
 
                             cmd = 'sox %s -r 16000 -b 16 -c 1 %s gain -n -3 silence -l 0 -1 0.2 0.1%%' % (wav_in, wav_out)
                             logging.debug(cmd)
